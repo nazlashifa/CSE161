77b885c53755fd064ebb6cfb0423ed8bfe4667b0
nazlashifa <nazlasifasultana14@gmail.com>
2024-12-14 21:22:16 +0600

    Add files via upload

--- lab01.c
+++ lab01.c
@@ -1,18 +1,30 @@
-#include<stdio.h>
 
-int main (void)
-{
-    int x;
-    printf("Enter a year:");
-    scanf("%d", &x);
-    if (x%4==0)
-    {
-        printf("Yes,It is leap year");
-    }
-    else
-    {
-        printf("No, It is not leap year");
-    }
+
+    #include <stdio.h>
+
+int main() {
+    int N, M;
+
+    // Input values for N and M
+    printf("Enter the value of N: ");
+    scanf("%d", &N);
+
+    printf("Enter the value of M: ");
+    scanf("%d", &M);
+
+
+
+     int count = 0;
+    for (int i = 1; i <= M; i++) {
+        if (i % N == 0) {
+            count++;
+        }
+      }
+
+    printf("The number of values divisible by %d that are less than or equal to %d is: %d\n", N, M, count);
+
+
+
 return 0;
 }
 
